1c0e35d70f5aa5d3241a369bb3312045d860fef9
IhsaneASAKOUR01 <120419470+IhsaneASAKOUR01@users.noreply.github.com>
2025-07-03 11:41:28 +0100

    app.py

--- CVs_REFs_adapter/app.py
+++ CVs_REFs_adapter/app.py
@@ -35,7 +35,7 @@ if "resume_results" not in st.session_state:
     st.session_state["resume_results"] = []
 
 def run_app():
-    st.title("AO Adapter: REFs & CVs 🤖")
+    st.title("AO Adapter: REFs & CVs")
 
     ao_title = st.text_input("AO Title", placeholder="Enter the AO title...")
     uploaded_ref = st.file_uploader("Upload Reference .docx file", type=["docx"], key="ref")
